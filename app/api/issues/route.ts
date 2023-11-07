2259f3d786f4e0e6de46ddc9bc5eeaf348bd8dfe
73rax <alexanderpaok11@gmail.com>
2023-11-07 14:50:48 +0000

    Build API endpoint for create issue

---
+++ app/api/issues/route.ts
@@ -0,0 +1,32 @@
+import prisma from "@/prisma/client";
+import { NextRequest, NextResponse } from "next/server";
+import z from "zod";
+
+const createIssueSchema = z.object({
+  title: z.string().min(1).max(255),
+  description: z.string().min(1),
+});
+
+// Make a post request
+export async function POST(request: NextRequest) {
+  // take the request and save it to a body
+  const body = await request.json();
+
+  // validate the data using zod
+  const validation = createIssueSchema.safeParse(body);
+
+  // if validation fails return error with status 400
+  if (!validation.success)
+    return NextResponse.json(validation.error.errors, {status: 400});
+
+  // create a new Issue with title and description in the database
+  const newIssue = await prisma.issue.create({
+    data: {
+      title: body.title,
+      description: body.description,
+    },
+  });
+
+  // return it to the client
+  return NextResponse.json(newIssue, { status: 201 });
+}
