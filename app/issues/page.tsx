d291b6e4d263f28c17b11fe3414b47a1748a8b55
73rax <alexanderpaok11@gmail.com>
2023-11-12 13:01:11 +0000

    Build the edit issue page

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -1,10 +1,15 @@
+import React from "react";
+import { Button } from "@radix-ui/themes";
+import Link from "next/link";
 import prisma from "@/prisma/client";
 import { Table } from "@radix-ui/themes";
-import { IssueStatusBadge, Link } from "@/app/components";
+import IssueStatusBadge from "../components/IssueStatusBadge";
+import delay from "delay";
 import IssueActions from "./IssueActions";
 
 const IssuesPage = async () => {
   const issues = await prisma.issue.findMany();
+  await delay(2000);
 
   return (
     <div>
