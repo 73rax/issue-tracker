3ae75b0a5ff8a42ff64081df8b9bceb129316e29
73rax <alexanderpaok11@gmail.com>
2023-11-13 15:30:01 +0000

    Enhance API to assign issues to users

--- app/api/issues/[id]/route.ts
+++ app/api/issues/[id]/route.ts
@@ -1,4 +1,4 @@
-import { issueSchema } from "@/app/ValidationSchema";
+import { patchIssueSchema } from "@/app/ValidationSchema";
 import authOptions from "@/app/auth/AuthOptions";
 import prisma from "@/prisma/client";
 import { getServerSession } from "next-auth";
@@ -12,11 +12,21 @@ export async function PATCH(
   if (!session) return NextResponse.json({}, { status: 401 });
 
   const body = await request.json();
+  const { assignedToUserId, title, description } = body;
 
-  const validation = issueSchema.safeParse(body);
+  const validation = patchIssueSchema.safeParse(body);
 
   if (!validation.success)
     return NextResponse.json(validation.error.format(), { status: 400 });
+
+  if (assignedToUserId) {
+    const user = await prisma.user.findUnique({
+      where: { id: assignedToUserId },
+    });
+
+    if (!user)
+      return NextResponse.json({ error: "Invalid user." }, { status: 400 });
+  }
 
   const issue = await prisma.issue.findUnique({
     where: {
@@ -33,8 +43,9 @@ export async function PATCH(
   const updatedIssue = await prisma.issue.update({
     where: { id: issue.id },
     data: {
-      title: body.title,
-      description: body.description,
+      title,
+      description,
+      assignedToUserId,
     },
   });
 
