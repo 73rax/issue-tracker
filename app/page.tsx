84312f50e3b3aaa66a320059a5a039225f72187a
73rax <alexanderpaok11@gmail.com>
2023-11-14 01:13:36 +0000

    Add pagination component and style

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,5 @@
-
+import Pagination from "./components/Pagination";
 
 export default function Home() {
-  return (
-    <div>Hello World</div>
-  )
+  return <Pagination itemCount={20} pageSize={5} currentPage={4} />;
 }
