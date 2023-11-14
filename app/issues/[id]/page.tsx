5a6ebe5ff66c442630d3fbf5253c1287a6b8f437
73rax <alexanderpaok11@gmail.com>
2023-11-14 15:33:41 +0000

    Optimise performance using react cahce

--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -7,10 +7,15 @@ import DeleteIssueButton from "./DeleteIssueButton";
 import { getServerSession } from "next-auth";
 import authOptions from "@/app/auth/AuthOptions";
 import AssigneeSelect from "./AssigneeSelect";
+import { cache } from "react";
 
 interface Props {
   params: { id: string };
 }
+
+const fetchUser = cache((issueId: number) =>
+  prisma.issue.findUnique({ where: { id: issueId } })
+);
 
 const IssueDetailPage = async ({ params }: Props) => {
   const session = await getServerSession(authOptions);
@@ -18,9 +23,7 @@ const IssueDetailPage = async ({ params }: Props) => {
   // Make sure that id provided in params is number and not anything else.
   // if (typeof params.id !== "number") notFound();
 
-  const issue = await prisma.issue.findUnique({
-    where: { id: parseInt(params.id) },
-  });
+  const issue = await fetchUser(parseInt(params.id));
 
   if (!issue) return notFound();
 
@@ -43,10 +46,8 @@ const IssueDetailPage = async ({ params }: Props) => {
 };
 
 export async function generateMetadata({ params }: Props) {
-  const issue = await prisma.issue.findUnique({
-    where: { id: parseInt(params.id) },
-  });
-
+  const issue = await fetchUser(parseInt(params.id));
+  
   return {
     title: issue?.title,
     description: "Details of issue " + issue?.id,
